ea390a66d02d57f0d1b286c3408eea6cf9667881
korzun1993 <korzun1993@mail.ru>
2013-07-28 13:25:23 +0300

    Project created

---
+++ eNewer/TMAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  TMAppDelegate.h
+//  eNewer
+//
+//  Created by Vlad Korzun on 28.07.13.
+//  Copyright (c) 2013 Vlad Korzun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ eNewer/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  eNewer
+//
+//  Created by Vlad Korzun on 28.07.13.
+//  Copyright (c) 2013 Vlad Korzun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TMAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TMAppDelegate class]));
+    }
+}
